85ea4e8dd623ab52b2e4ea0c52753c9911d6dfc6
naqprog <naknobprog@gmail.com>
2023-12-23 03:13:32 +0900

    ・間違えた問題から出題するモジュールのバグ修正

--- src/app/models/response.rb
+++ src/app/models/response.rb
@@ -2,5 +2,5 @@ class Response < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :question
 
-  enum :test_format, { default: 0, etc: 1 }
+  enum :test_format, { chengyu: 0, mean: 1 }
 end
