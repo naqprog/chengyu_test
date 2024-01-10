69167f5e9fada0c4a158cec20a2df1cf2684b166
naqprog <naknobprog@gmail.com>
2024-01-11 05:04:51 +0900

    ・ユーザのサインイン、退会に関するテストの作成 ・退会時に再確認用のポップアップを表示するように修正 ・その他、リンクやボタンにIDを追加

---
+++ src/spec/system/no_sign_in_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.feature "ログイン無しでできることの確認", type: :system, js: true do
+  scenario 'look chengyu' do
+    # 成語一覧画面を見る
+    visit root_path
+    click_on '成語一覧を見る'
+    expect(page).to have_content '一心一意'
+  end
+  scenario 'test chengyu jiantizi' do
+    # 成語を聞く・簡体字　でテストを実施
+    visit root_path
+    find('#chengyutest_jiantizi').click
+    find('#answer0').click
+    find('#answer1').click
+    find('#answer2').click
+    find('#answer3').click
+    click_on '回答する'
+    expect(page).to have_content '[入力された答え]' # 正誤判定画面
+    expect(page).to have_content '※ログインするとお気に入り問題等が保存できます' # ログインしてない証
+  end
+  scenario 'test chengyu fantizi' do
+    # 成語を聞く・繁体字　でテストを実施
+    visit root_path
+    find('#chengyutest_fantizi').click
+    find('#answer0').click
+    find('#answer1').click
+    find('#answer2').click
+    find('#answer3').click
+    click_on '回答する'
+    expect(page).to have_content '[入力された答え]' # 正誤判定画面
+    expect(page).to have_content '※ログインするとお気に入り問題等が保存できます' # ログインしてない証
+  end
+  scenario 'test mean jiantizi' do
+    # 成語を聞く・簡体字　でテストを実施
+    visit root_path
+    find('#meantest_jiantizi').click
+    find('#flexRadio2').choose
+    click_on '回答する'
+    expect(page).to have_content '[入力された答え]' # 正誤判定画面
+    expect(page).to have_content '※ログインするとお気に入り問題等が保存できます' # ログインしてない証
+  end
+  scenario 'test mean fantizi' do
+    # 成語を聞く・簡体字　でテストを実施
+    visit root_path
+    find('#meantest_fantizi').click
+    find('#flexRadio2').choose
+    click_on '回答する'
+    expect(page).to have_content '[入力された答え]' # 正誤判定画面
+    expect(page).to have_content '※ログインするとお気に入り問題等が保存できます' # ログインしてない証
+  end
+
+end
